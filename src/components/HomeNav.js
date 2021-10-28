29a73e277c8186df18421486c2808a632ebd8099
MonikaJedrzejak <jedrzejakmonika@tlen.pl>
2021-10-28 19:43:40 +0200

    Navbar structure

--- src/components/HomeNav.js
+++ src/components/HomeNav.js
@@ -1,9 +1,37 @@
-import React from 'react'
+import React from "react";
 
 export default function HomeNav() {
-    return (
-        <div>
-            
-        </div>
-    )
+	return (
+		<header className="home-nav">
+			<nav className="home-nav-login">
+				<ul className="home-menu">
+					<li className="home-menu-item">
+						<a className="btn login-btn" href="#LogIn">Zaloguj</a>
+					</li>
+					<li className="home-menu-item">
+						<a className="btn login-btn"href="#SignIn">Załóż konto</a>
+					</li>
+				</ul>
+			</nav>
+			<nav className="home-nav-menu">
+				<ul className="home-menu">
+					<li className="home-menu-item">
+						<a className="btn menu-btn" href="#Start">Start</a>
+					</li>
+					<li className="home-menu-item">
+						<a className="btn menu-btn" href="#About">O co chodzi?</a>
+					</li>
+					<li className="home-menu-item">
+						<a className="btn menu-btn" href="#AboutUs">O nas</a>
+					</li>
+					<li className="home-menu-item">
+						<a className="btn menu-btn" href="#Organizations">Fundacja i organizacje</a>
+					</li>
+					<li className="home-menu-item">
+						<a className="btn menu-btn" href="#Contact">Kontakt</a>
+					</li>
+				</ul>
+			</nav>
+		</header>
+	);
 }
