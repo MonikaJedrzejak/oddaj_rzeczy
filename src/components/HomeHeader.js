5ba1360534b96e3c1bc97cd9cad87c26d10c4527
MonikaJedrzejak <jedrzejakmonika@tlen.pl>
2021-10-29 13:31:17 +0200

    styles for header

--- src/components/HomeHeader.js
+++ src/components/HomeHeader.js
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from 'react';
 
 export default function HomeHeader() {
     return (
-        <div>
-            <p>Hello</p>
+        <div className="home-header-container">
+        <div className="header-image"></div>
+        <div className="header-text-container">
+        <h2 className="header-title">Zacznij pomagać!</h2>
+        <h2 className="header-title">Oddaj niechciane rzeczy w zaufane ręce</h2>
+        <div className="decoration"></div>
+        <div className="header-btn-container">
+            <button className="btn btn-header"><a href="#Home" >ODDAJ RZECZY</a></button>
+            <button className="btn btn-header"><a href="#Home" >ZORGANIZUJ ZBIÓRKĘ</a></button>
+        </div>
+        </div>
         </div>
     )
 }
--- src/components/HomeNav.js
+++ src/components/HomeNav.js
@@ -5,29 +5,29 @@ export default function HomeNav() {
 		<header className="home-nav">
 			<nav className="home-nav-login">
 				<ul className="home-menu">
-					<li className="home-menu-item">
+					<li className="home-menu-item login-item">
 						<a className="btn login-btn" href="#LogIn">Zaloguj</a>
 					</li>
-					<li className="home-menu-item">
+					<li className="home-menu-item login-item">
 						<a className="btn login-btn"href="#SignIn">Załóż konto</a>
 					</li>
 				</ul>
 			</nav>
 			<nav className="home-nav-menu">
 				<ul className="home-menu">
-					<li className="home-menu-item">
+					<li className="home-menu-item menu-item">
 						<a className="btn menu-btn" href="#Start">Start</a>
 					</li>
-					<li className="home-menu-item">
+					<li className="home-menu-item menu-item">
 						<a className="btn menu-btn" href="#About">O co chodzi?</a>
 					</li>
-					<li className="home-menu-item">
+					<li className="home-menu-item menu-item">
 						<a className="btn menu-btn" href="#AboutUs">O nas</a>
 					</li>
-					<li className="home-menu-item">
+					<li className="home-menu-item menu-item">
 						<a className="btn menu-btn" href="#Organizations">Fundacja i organizacje</a>
 					</li>
-					<li className="home-menu-item">
+					<li className="home-menu-item menu-item">
 						<a className="btn menu-btn" href="#Contact">Kontakt</a>
 					</li>
 				</ul>
